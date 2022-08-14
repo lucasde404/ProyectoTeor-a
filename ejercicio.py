a9e9d0dcb261122be2ac93ad1ecfc92ac396496a
Christian Murua <96192358+cristianmurua18@users.noreply.github.com>
2022-08-14 18:22:35 -0300

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,72 @@
+
+# 7)  Crear una tupla en Python con el nombre de “Historial3” la cual contenga los siguientes valores:
+
+#           9530, 4120, 4580, 1500, 890,7516,426
+
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Lennon”. 
+# Calcular el promedio de gasto en pesos por atención de “Lennon” mostrándolo en pantalla. 
+# Si el promedio es mayor a 4500 indicar con un mensaje “Se ha excedido del gasto promedio para su mascota”.
+
+# ----------------------------------------------------------------------------------------------------------
+# CODIGO
+# Historial3 = (9530, 4120, 4580, 1500, 890,7516,426)
+
+# acumulador = 0
+# for i in Historial3:
+#     acumulador += i
+
+# promedio = acumulador / len(Historial3)
+
+# print(promedio)
+
+# if promedio > 4500:
+#     print("Se ha excedido del gasto promedio para su mascota.")
+# else:
+#     print("El promedio de gastos de Lennon esta contemplado.")
+
+
+
+# 8) Crear una tupla en Python con el nombre de “Historial4” la cual contenga los siguientes valores:
+
+#           7510, 7960, 76180, 800, 4100
+
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Olivia”. 
+# Cree una función para determinar el valor mínimo de atención gastada en “Olivia”  mostrándolo en pantalla.
+
+# ----------------------------------------------------------------------------------------------------------
+# CODIGO
+# def valor_min ():
+#     Historial4 = (7510, 7960, 76180, 800, 4100)
+#     print(min(Historial4))
+
+# valor_min()
+
+
+
+# 9) Crear una tupla en Python con el nombre de “Historial5” la cual contenga los siguientes valores:
+
+#           8520, 9510, 7530, 3570, 1590
+
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Toto”. 
+# Crear una función para determinar el valor máximo de atención gastada en “Toto”.
+
+# ---------------------------------------------------------------------------------------------------------
+# CODIGO
+# def valor_max ():
+#     Historial5 = (8520, 9510, 7530, 3570, 1590)
+#     print(max(Historial5))
+
+# valor_max()
+
+
+# 10) Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
+
+#           Juan,  Mario,  Ariel,  Josefina,  Marianella.
+
+# Ordenarla alfabéticamente y mostrarla por pantalla.
+
+# ----------------------------------------------------------------------------------------------------
+# CODIGO
+# Clientes = ["Juan",  "Mario", "Ariel", "Josefina", "Marianella"]
+# Clientes.sort()
+# print(Clientes)
---
+++ peluqueriacanina.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE PeluqueriaCanina;
+
+USE PeluqueriaCanina;
+
+CREATE TABLE Dueño(
+DNI int not null,
+Nombre varchar(45) not null,
+Apellido varchar(45) not null,
+Telefono int not null,
+Direccion varchar(45) null,
+CONSTRAINT pk_DNI PRIMARY KEY(DNI)
+);
+
+CREATE TABLE Perro(
+ID_Perro int not null,
+Nombre varchar(45) not null,
+Fecha_nac date not null,
+Sexo varchar(25) null,
+DNI_dueño int,
+CONSTRAINT pk_ID_Perro PRIMARY KEY(ID_Perro),  
+CONSTRAINT fk_DNI_Dueño FOREIGN KEY (DNI_dueño) REFERENCES Dueño (DNI)
+);
+
+
+CREATE TABLE Historial(
+ID_Historial int not null,
+Fecha datetime not null,
+Perro int not null,
+Descripcion varchar(45) not null,
+Monto int not null,
+CONSTRAINT pk_ID_Historial PRIMARY KEY(ID_Historial),
+CONSTRAINT fk_Perro FOREIGN KEY (Perro) REFERENCES Perro (ID_Perro) 
+);
+
+INSERT INTO Dueño (DNI, Nombre, Apellido, Telefono, Direccion)
+VALUES (28957346,  "Juan",  "Perez", 4789689,  "Belgrano 101"); 
+
+INSERT INTO Dueño (DNI, Nombre, Apellido, Telefono, Direccion)
+VALUES (23546987, "Maria", "Perez", 4789689, "Pueyrredon 811");
+
+INSERT INTO Perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_Dueño)
+VALUES (13,  "Puppy",  "2022-12-12", "Macho", 28957346);
+
+INSERT INTO Perro (ID_Perro, Nombre, Fecha_nac, sexo, DNI_Dueño)
+VALUES (14,  "Fido",  "2022-12-11", "Macho", 23546987);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (1, "2022-07-05", 13, "Baño", 2350);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (2, "2022-07-07", 13, "Comida", 5960);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (3, "2022-07-22", 13, "Guardaeria", 23000);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (4, "2022-07-27", 13, "Corte uñas", 1000);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (5, "2022-07-29", 13, "Otros", 8900);
+
+INSERT INTO Historial (ID_Historial, Fecha, Perro, Descripcion, Monto)
+VALUES (6, "2022-08-3", 14, "Guarderia", 23500);
+
+SELECT * FROM Historial;
+
+SELECT Monto FROM Historial WHERE Fecha between "2022-07-01" and "2022-07-31"; 
